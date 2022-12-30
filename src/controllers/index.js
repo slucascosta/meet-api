ad2db390b748f3a8cd02461d2a02c1634e8ddcbb
Lucas Costa <lucas.costa@waproject.com.br>
2022-12-30 03:20:41 -0300

    test6

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,5 +1,7 @@
 import ChatController from "./chat.js";
+import HelloController from "./hello.js";
 
 export default function (httpServer) {
   new ChatController(httpServer);
+  new HelloController(httpServer);
 };
